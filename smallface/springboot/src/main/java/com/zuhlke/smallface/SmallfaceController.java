c3eb0e0b4a4eead2d7022a4e70695e598057bee6
Hudson, Philip <Philip.Hudson@zuhlke.com>
2018-12-06 09:35:46 +0000

    (chore): add missing dependency for JDK 10

--- smallface/springboot/src/main/java/com/zuhlke/smallface/SmallfaceController.java
+++ smallface/springboot/src/main/java/com/zuhlke/smallface/SmallfaceController.java
@@ -1,17 +1,8 @@
 package com.zuhlke.smallface;
 
-import com.zuhlke.smallface.entities.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
